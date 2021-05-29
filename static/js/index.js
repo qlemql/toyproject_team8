9e305d8995967382b99ca4a659745250e45c53ea
qlemql <busgamer@naver.com>
2021-05-29 23:27:21 +0900

    프론트 백앤드 연결
    총 방문자 수 / 일일 방문자 수 연결

--- static/js/index.js
+++ static/js/index.js
@@ -128,35 +128,41 @@ prevBtn.addEventListener("click", function () {
 // 백엔드 js
 
 $(document).ready(function () {
-            showCounts();
-            showTotalCounts();
-        });
+    showCounts();
+    showTotalCounts();
+});
 
-        function showTotalCounts() {
-            $.ajax({
-                type: "GET",
-                url: "/api/todayCounts",
-                data: {},
-                success: function (response) {
-                    let todayCounts = response['today_counts']
-                    for (let i = 0; i < todayCounts.length; i++) {
-                        let today = todayCounts[i]['todayCounts']
-                        console.log(today);
-                    }
-                }
-            })
+function showTotalCounts() {
+    $.ajax({
+        type: "GET",
+        url: "/api/todayCounts",
+        data: {},
+        success: function (response) {
+            let todayCounts = response['today_counts']
+            for (let i = 0; i < todayCounts.length; i++) {
+                let today = todayCounts[i]['todayCounts']
+                console.log(today);
+                let temp_html = `<div class="totady">일일 방문자 수 : ${today}명</div>`
+                $('#counter').append(temp_html);
+            }
         }
+    })
+}
 
-        function showCounts() {
-            $.ajax({
-                type: "GET",
-                url: "/api/totalCounts",
-                data: {},
-                success: function (response) {
-                    let totalCounts = response['total_counts']['Counts']
-                    console.log(totalCounts);
-                    let temp_html = ``
+function showCounts() {
+    $.ajax({
+        type: "GET",
+        url: "/api/totalCounts",
+        data: {},
+        success: function (response) {
+            let totalCounts = response['total_counts']
+            for (let i = 0; i < totalCounts.length; i++) {
+                let total = totalCounts[i]['Counts']
+                console.log(total);
+                let temp_html = `<div class="visitors">총 방문자 수 : ${total}명</div>`
+                $('#counter').append(temp_html);
+            }
 
-                }
-            })
         }
+    })
+}
