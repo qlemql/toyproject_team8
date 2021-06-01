952a74d80fafa1207dab05f24bf89c33b430a483
qlemql <taehyun_fe@naver.com>
2021-06-01 14:17:29 +0900

    기타 수정
    배경 이미지 추가 및 dark모드 홈페이지 진입시 light모드가 먼저 뜨도록 변경
    qna margin 조절
    시작하기 버튼, background-image css 수정
    시작하기 버튼 클릭 시 qna 섹션으로 이동

--- static/js/intro.js
+++ static/js/intro.js
@@ -2,7 +2,9 @@
 
 // dark mode
 const btn = document.querySelector(".dark__mode");
+const startBtn = document.querySelector('.start');
 const currentTheme = localStorage.getItem("theme");
+const body = document.querySelector('body');
 
 if (currentTheme == "dark") {
   document.body.classList.add("dark-theme");
@@ -12,7 +14,21 @@ btn.addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
   let theme = "light";
   if (document.body.classList.contains("dark-theme")) {
+    startBtn.style.color = "#121212";
+    startBtn.style.backgroundColor = '#eee';
+    startBtn.style.border = '1px solid #121212';
+    body.style.backgroundImage = 'url(https://i.imgur.com/mZT110x.png)';
+    body.style.backgroundSize = 'cover';
+    body.style.backgroundPosition = 'center';
+  }
+  else {
     theme = "dark";
+    startBtn.style.color = "#eee";
+    startBtn.style.backgroundColor = '#121212';
+    startBtn.style.border = '1px solid #eee';
+    body.style.backgroundImage = 'url(https://i.imgur.com/oYS2gZf.png)';
+    body.style.backgroundSize = 'cover';
+    body.style.backgroundPosition = 'center';
   }
   localStorage.setItem("theme", theme);
 });
@@ -28,18 +44,17 @@ btn.onclick = function () {
   iconSun.style.justifyContent = "center";
   iconMoon.style.justifyContent = "center";
 
-  if (iconMoon.style.display == "flex") {
-    iconSun.style.display = "flex";
-    iconMoon.style.display = "none";
-  } else {
-    iconMoon.style.display = "flex";
+  if (iconSun.style.display == "flex") {
     iconSun.style.display = "none";
+    iconMoon.style.display = "flex";
+  } else {
+    iconMoon.style.display = "none";
+    iconSun.style.display = "flex";
   }
 };
 
-const startBtn = document.querySelector(".start");
 const firstPage = document.querySelector(".section1");
-// const secondPage = document.querySelector('section2');
+const secondPage = document.querySelector('.section2');
 // const secondPage = document.querySelector('section3');
 const lastPage = document.querySelector(".all");
 
@@ -52,16 +67,25 @@ const nameInput = document.querySelector('input');
 const userName = document.querySelector('.userName');
 
 function handleName (event) {
-  firstPage.style.display = "none";
-  lastPage.style.display = "flex";
-  // 새로고침 방지
-  event.preventDefault();
-  // 사용자가 입력한 이름값
-  const currentValue = nameInput.value;
-  // 사용자가 입력한 이름을 보여주는 것
-  paintToName(currentValue);
-  // enter 또는 시작하기 눌렀을때 input창 초기화
-  nameInput.value = "";
+  if (nameInput.value == ''){
+    alert("이름을 입력해주세요.");
+    firstPage.style.display = "flex";
+    secondPage.style.display = 'none';
+  } else{
+    firstPage.style.display = "none";
+    secondPage.style.display = 'flex';
+    secondPage.style.justifyContent = "center";
+    secondPage.style.alignItems = 'center';
+    secondPage.style.flexDirection = 'column';
+    // 새로고침 방지
+    event.preventDefault();
+    // 사용자가 입력한 이름값
+    const currentValue = nameInput.value;
+    // 사용자가 입력한 이름을 보여주는 것
+    paintToName(currentValue);
+    // enter 또는 시작하기 눌렀을때 input창 초기화
+    nameInput.value = "";
+  }
 }
 
 function paintToName(text) {
